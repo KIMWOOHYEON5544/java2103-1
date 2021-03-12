0fb97215543635080b6ea3766f129e378a51c14e
swcodingschool <swcodingschool@gmail.com>
2021-03-12 14:21:49 +0900

    Initiate project A01

---
+++ A01_JavaBasics/src/com/example/day01/A011_HelloWorld.java
@@ -0,0 +1,19 @@
+package com.example.day01;
+/*==========================================================================================
+강의 진행, 프로젝트/패키지/클래스명명 규칙
+프로젝트명 : 수업 단계 표시. JavaBaisc / JavaOOP / JavaGui / JavaJDBC / JavaAppl / JavaWeb
+패키지명 : com.example.일차수 / com.practice.일차수 / com.report.일차수 / com.zup.일차수
+클래스명 : A:수업준비 예제, B:수업 중 시연예제
+------------------------------------------------------------------------------------------
+[학습목표] 
+1. 자바 프로그래밍 작성 및 실행 방법을 이해하고 실습한다.
+2. 통합개발환경IDE의 활용방법을 익힌다. 
+===========================================================================================*/
+public class A011_HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World.");
+	}
+
+}
