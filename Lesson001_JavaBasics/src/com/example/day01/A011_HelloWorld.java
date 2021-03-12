1d78726fdabfcdb7c8b6e612329b25394ef7a96b
swcodingschool <swcodingschool@gmail.com>
2021-03-12 15:12:48 +0900

    update code

--- Lesson001_JavaBasics/src/com/example/day01/A011_HelloWorld.java
+++ Lesson001_JavaBasics/src/com/example/day01/A011_HelloWorld.java
@@ -1,8 +1,8 @@
 package com.example.day01;
 /*==========================================================================================
   ★★★ swcodingschool's Coding Class ★★★
-  프로젝트/패키지/클래스명명 규칙
-  프로젝트명 : 수업 단계 표시. JavaBaisc / JavaOOP / JavaGui / JavaJDBC / JavaAppl / JavaWeb
+  프로젝트명 : 수업 단계 표시. 
+       JavaBaisc / JavaOOP / JavaGui / JavaJDBC / Application / SpringFramework / SpringBoot
   패키지명 : com.example.일차수 / com.practice.일차수 / com.report.일차수 / com.zup.일차수
   클래스명 : A:수업준비 예제, B:수업 중 시연예제 / 일차수+일련번호 / 예제내용
 ------------------------------------------------------------------------------------------
